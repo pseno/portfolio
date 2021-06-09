5c55101c81e91c93782c646116b09dc420e170ad
pseno <pasalicseno@hotmail.com>
2021-06-09 18:51:15 +0200

    Add line

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -50,7 +50,7 @@ function Portfolio() {
                     href="https://treasure-hunter00.netlify.app/"
                     target="_blank"
                   >
-                    <img src="pages/pics/1.svg" />
+                    <img src="1.png" />
                   </a>
                 </div>
                 <div>
